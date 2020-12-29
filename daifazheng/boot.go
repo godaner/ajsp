94785d15fc6db60f61a86b1743e0de26889a93d7
godaner <1138829222@qq.com>
2020-12-29 11:09:29 +0800

    many worker

--- daifazheng/boot.go
+++ daifazheng/boot.go
@@ -9,47 +9,77 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
 var httpAddr, auth, route string
-var n, w uint64
+var n, w, wt uint64
+var doCount, suCount uint64
 
 // 第四个 待制证
 func main() {
 	flag.StringVar(&httpAddr, "ha", "http://59.225.201.162:8086", "http addr")
 	flag.StringVar(&auth, "auth", "", "http Authorization")
-	flag.Uint64Var(&n, "n", 50, "exec number")
+	flag.Uint64Var(&n, "n", 50, "fetch number")
 	flag.Uint64Var(&w, "w", 1, "worker")
 	flag.StringVar(&route, "route", "", "route")
+	flag.Uint64Var(&wt, "wt", 5, "no data wait time , sec")
 	flag.Parse()
-	if httpAddr == "" || auth == "" || n == 0 {
+	if httpAddr == "" || auth == "" || n == 0 || w == 0 || route == "" || wt == 0 {
 		flag.PrintDefaults()
 		return
 	}
+	newHC := func() *http.Client {
+		return &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+			// CheckRedirect: op[0].CheckRedirect,
+			// Jar:           op[0].Jar,
+			Timeout: time.Duration(60) * time.Second,
+		}
+	}
+	// read count
+
+	// sync count
+	rsc := make(chan interface{}, n)
+	// fetch
+	go func() {
+		hc := newHC()
+		// loop fetch
+		for ; ; {
+			rs := fetch(hc, wt)
+			for _, r := range rs {
+				rsc <- r
+			}
+			if len(rs) == 0 {
+				<-time.After(time.Duration(wt) * time.Second) // 等待n s
+			}
+		}
+	}()
+	// do
 	for i := 0; i < int(w); i++ {
+		// many worker
 		go func() {
+			hc := newHC()
+			// loop do
 			for ; ; {
-				do()
+				do(hc, <-rsc)
 			}
 		}()
 	}
 	select {}
 }
-func do() {
+
+// fetch
+func fetch(hc *http.Client, wt uint64) (rs []interface{}) {
 	defer func() {
 		if err := recover(); err != nil {
-			log.Println("PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC , err is ", err)
+			log.Println("fetch : PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC , err is ", err)
 		}
 	}()
-	hc := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-		// CheckRedirect: op[0].CheckRedirect,
-		// Jar:           op[0].Jar,
-		Timeout: time.Duration(60) * time.Second,
-	}
+
 	req, err := http.NewRequest("POST", httpAddr+"/api/approval/wf/task/upcoming", bytes.NewBufferString(`{"page":1,"rows":`+fmt.Sprint(n)+`,"orderBy":{"updateTime":"desc"},"affairName":null,"likeMap":{},"between":{},"in":{"bizStatus":["81"]}}`))
 	if err != nil {
 		panic(err)
@@ -74,36 +104,43 @@ func do() {
 		panic(err)
 	}
 	log.Printf("get record success , result is : %+v!", result)
-	rs := result["data"].(map[string]interface{})["records"].([]interface{})
-	if len(rs) == 0 {
-		<-time.After(10 * time.Second) // 等待10s
+	rs = result["data"].(map[string]interface{})["records"].([]interface{})
+	return rs
+}
+
+// do
+func do(hc *http.Client, r interface{}) {
+	index := atomic.AddUint64(&doCount, 1)
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("do : PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC PANIC , err is ", err)
+		}
+	}()
+	rm := r.(map[string]interface{})
+	log.Printf("do record start , index is : %v , record is : %+v!", index, rm)
+	shardKey := rm["shardKey"].(string)
+	affairId := rm["busiId"].(string)
+	req, err := http.NewRequest("POST", httpAddr+"/api/approval/dth/affair/submitAffair", bytes.NewBufferString(`{"affairId":"`+affairId+`","shardKey":"`+shardKey+`","handleType":9,"auditAdvice":1,"auditAdviceInfo":null}`))
+	if err != nil {
+		log.Printf("do record err 1 , index is : %v , err is : %v!", index, err)
+		return
 	}
-	for index, r := range rs {
-		rm := r.(map[string]interface{})
-		log.Printf("handle record start , index is : %v , record is : %+v!", index, rm)
-		shardKey := rm["shardKey"].(string)
-		affairId := rm["busiId"].(string)
-		req, err := http.NewRequest("POST", httpAddr+"/api/approval/dth/affair/submitAffair", bytes.NewBufferString(`{"affairId":"`+affairId+`","shardKey":"`+shardKey+`","handleType":9,"auditAdvice":1,"auditAdviceInfo":null}`))
-		if err != nil {
-			log.Printf("handle record err 1 , index is : %v , err is : %v!", index, err)
-			continue
-		}
-		req.Header.Set("Authorization", auth)
-		req.Header.Set("Content-Type", "application/json")
-		req.AddCookie(&http.Cookie{Name: "route", Value: route})
-		resp, err := hc.Do(req)
-		if err != nil {
-			log.Printf("handle record err 2 , index is : %v , err is : %v!", index, err)
-			continue
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		if body != nil {
-			resp.Body.Close()
-		}
-		if err != nil {
-			log.Printf("handle record err 3 , index is : %v , err is : %v!", index, err)
-			continue
-		}
-		log.Printf("handle record end , index is : %v , res is : %v!", index, string(body))
+	req.Header.Set("Authorization", auth)
+	req.Header.Set("Content-Type", "application/json")
+	req.AddCookie(&http.Cookie{Name: "route", Value: route})
+	resp, err := hc.Do(req)
+	if err != nil {
+		log.Printf("do record err 2 , index is : %v , err is : %v!", index, err)
+		return
 	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if body != nil {
+		resp.Body.Close()
+	}
+	if err != nil {
+		log.Printf("do record err 3 , index is : %v , err is : %v!", index, err)
+		return
+	}
+	log.Printf("do record end , index is : %v , res is : %v!", index, string(body))
+	atomic.AddUint64(&suCount, 1)
 }
